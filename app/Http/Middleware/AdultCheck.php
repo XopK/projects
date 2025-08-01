2159294b4369dac1aa2592db32055f55c4993a78
XopK <dmahmutov12@gmail.com>
2025-08-01 20:32:50 +0500

    age verify

---
+++ app/Http/Middleware/AdultCheck.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\Group;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdultCheck
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response) $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $group = $request->route('group');
+
+        if ($group->age_verify) {
+            $user = Auth::user();
+
+            if (!$user->isAdult()) {
+                abort(403);
+            }
+        }
+
+        return $next($request);
+    }
+}
